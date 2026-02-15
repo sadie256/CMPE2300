0b573af6e9c59ed117f25374f5c810f1137ff027
Sadie Pretzlaff <sadiepretzlaff@gmail.com>
2026-02-15 10:37:15 -1200

    Added GDIDrawer and Package class members

--- Lab01_SadieP/Package.cs
+++ Lab01_SadieP/Package.cs
@@ -8,5 +8,19 @@ namespace Lab01_SadieP
 {
     internal class Package
     {
+        private static Random _rnd = new Random();
+
+        private List<Point> _lines;
+
+        private int _id;
+
+        public int Id { get { return _id; } }
+
+        public Color _color { get; set; }
+
+        public Point Location { get; set; }
+
+
+
     }
 }
